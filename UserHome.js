5dad435fc4280d1da3c269bed5931768b11e0b68
gspreethi <88823470+gspreethi@users.noreply.github.com>
2021-08-23 15:41:07 +0530

    Create UserHome.js

---
+++ UserHome.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Button } from 'reactstrap'
+import {useHistory} from 'react-router-dom'
+import './UserHome.css'
+import carousal from '';
+
+function UserHome() {
+    let history= useHistory();
+    
+    return (
+        <div className="userHome">
+        <carousal/>
+        </div>
+        <div>
+        <h1>Browse to select the gift to ur loved ones</h1>
+            <Button className="adminHome__addAgift" onClick={()=>{history.push('/addABook')}}> Browse</Button>
+        </div>
+    )
+}
+
+export default AdminHome

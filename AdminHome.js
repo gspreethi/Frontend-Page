0a1f6901b5edcbfcf32a0fb064dd67d548cb6ce2
gspreethi <88823470+gspreethi@users.noreply.github.com>
2021-08-23 15:39:10 +0530

    Update AdminHome.js

--- AdminHome.js
+++ AdminHome.js
@@ -2,14 +2,18 @@ import React from 'react'
 import { Button } from 'reactstrap'
 import {useHistory} from 'react-router-dom'
 import './AdminHome.css'
+import carousal from '';
 
 function AdminHome() {
     let history= useHistory();
     
     return (
         <div className="adminHome">
-            <Button className="adminHome__addAgift" onClick={()=>{history.push('/addABook')}}> Add a Gift</Button>
-            <Button className="adminHome__deleteAgift" onClick={()=>{history.push('/deleteABook')}}> Delete a Gift</Button>
+        <carousal/>
+        </div>
+        <div>
+        <h1>You can  Add category and gifts, Delete any , update any here</h1>
+            <Button className="adminHome__addAgift" onClick={()=>{history.push('/addABook')}}> Maintain the products</Button>
         </div>
     )
 }

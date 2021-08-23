d2e911e5f1c241c3e2d34354d80d24b8934bd654
gspreethi <88823470+gspreethi@users.noreply.github.com>
2021-08-23 15:03:06 +0530

    Update ForgotPassword.js

--- ForgotPassword.js
+++ ForgotPassword.js
@@ -40,12 +40,13 @@ export default function ForgotPassword() {
     }
     return (
         <div>
-            <h1>Forgot Password</h1>
-            <form onSubmit={handleValidation}
-         style={{border:'1px solid blue',"padding":3,"width":500,"paddingLeft":30,fontFamily:'sans-serif',color:'darkgreen',alignItems:"center"}} >
-
-            
-         <div className="mb-3">
+      <div className="container pt-5">
+    <div className="row justify-content-sm-center pt-5">
+      <div className="col-sm-6 shadow round pb-3">
+        <h1 className="text-center pt-3 text-primary">Forgot your Password</h1>
+        <form  onSubmit={handleValidation}>
+                      
+         <div className="form-group">
           <label htmlFor="userId" className="form-label">user Id</label>
           <input type="number" className="form-control"  id="userId" 
           aria-describedby="userId" name="userId" 
@@ -53,60 +54,46 @@ export default function ForgotPassword() {
           
         </div>
 
-        {/* question 1     */}
-        <div className="mb-3">
-          <label htmlFor="question1" className="form-label">Security Question 1</label>
-          
-            <p id="question1">Who is your favourite hero ?</p>
-           
-         </div> 
+        <div className="form-group">
+          <label htmlFor="que1" className="form-label">Question-1</label>
+               <p id="que1">What is your first School Name?</p>
+          </div> 
 
 
-         <div className="mb-3">
+         <div className="form-group">
           <label htmlFor="answer1" className="form-label">Answer</label>
           <input type="text" className="form-control"  id="answer1" 
-          aria-describedby="answer1" name="answer1" 
-          onChange={(e)=>{setData({...data,securityanswer1:e.target.value})}} required />
-          <div id="answer1" className="form-text">We'll never share your answer with anyone else.</div>
-        </div>
+          aria-describedby="answer1"
+          onChange={(e)=>{setData({...data,securityAns1:e.target.value})}} required />
+          </div>
 
 
          {/* question 2     */}
-         <div className="mb-3">
-          <label htmlFor="question2" className="form-label">Security Question 2</label>
-         
-            <p id="question2">What is your favourite pet ?</p>
-            
-         </div> 
+         <div className="form-group">
+          <label htmlFor="question2" className="form-label">Question-2</label>
+               <p id="question2">What is your favourite pet animal?</p>
+           </div> 
 
-         <div className="mb-3">
+         <div className="form-group">
           <label htmlFor="answer2" className="form-label">Answer</label>
           <input type="text" className="form-control"  id="answer2" 
           aria-describedby="answer2" name="answer2" 
-          onChange={(e)=>{setData({...data,securityanswer2:e.target.value})}} required />
-          <div id="answer2" className="form-text">We'll never share your answer with anyone else.</div>
-        </div>
+          onChange={(e)=>{setData({...data,securityAns2:e.target.value})}} required />
+                 </div>
 
          {/* question 3     */}
-         <div className="mb-3">
+         <div className="form-group">
           <label htmlFor="question3" className="form-label">Security Question 3</label>
+               <p id="question3">What is your favourite Holiday destination?</p>
+            </div> 
           
-            <p id="question3">What is your favourite book ?</p>
-         
-         </div> 
-          
-        <div className="mb-3">
+        <div className="form-group">
           <label htmlFor="answer3" className="form-label">Answer</label>
           <input type="text" className="form-control"  id="answer3" 
           aria-describedby="answer3" name="answer3" 
           onChange={(e)=>{setData({...data,securityanswer3:e.target.value})}} required />
-          <div id="answer3" className="form-text">We'll never share your answer with anyone else.</div>
         </div>
 
-        
-
-        
-      
         <button type="submit" className="btn btn-success"  
          
         >Submit</button>
